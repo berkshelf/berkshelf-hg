6501d007892d49bf3f69b22d08ae3c626a450f0e
Seth Vargo <sethvargo@gmail.com>
2014-03-04 22:21:19 -0500

    Use aruba inprocess

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,14 @@
 require 'aruba/cucumber'
+require 'aruba/in_process'
 require 'berkshelf'
 
 Before do
+  Aruba::InProcess.main_class = Berkshelf::Cli::Runner
+  Aruba.process = Aruba::InProcess
+
   @aruba_timeout_seconds = 30
-  set_env('BERKSHELF_PATH', tmp_path)
+
+  ENV['BERKSHELF_PATH'] = tmp_path
 end
 
 def tmp_path
